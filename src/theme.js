009af7ecd805c5b7ca6ce7693e742557b64b2e63
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-25 00:13:04 +0500

    added theme file

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  
+
+      paddingHorizontal: 'px-4 md:px-10 lg:px-40 2xl:px-60',
+      paddingVertical: 'py-16 md:py-20 lg:py-24',
+      paddingBottom: 'pb-12 md:pb-20 lg:pb-24',
+
+   
+  };
+  
