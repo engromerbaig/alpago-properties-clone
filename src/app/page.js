f6ada86a02baa25bf3e06ac849483f9c69b1694d
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-24 15:33:40 +0500

    colors

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="max-w-4xl mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold mb-4">Welcome to JobSite</h1>
+    <div className="max-w-4xl bg-primary mx-auto px-4 py-10">
+      <h1 className="text-3xl text-green font-bold mb-4">Welcome to JobSite</h1>
       <p>This is a basic homepage for your job test project.</p>
     </div>
   );
