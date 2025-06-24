8fe7945b6502dcf92906ff71cd30900a0068e72f
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-24 18:22:16 +0500

    clean

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import Hero from '../components/Hero';
-import Projects from '../components/Projects';
-import Team from '../components/Team';
-import Experience from '../components/Experience';
-import News from '../components/News';
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
+import Team from '@/components/Team';
+import Experience from '@/components/Experience';
+import News from '@/components/News';
 
 // Home page component for JobSite
 export default function Home() {
   return (
-    <div className="max-w-4xl mx-auto px-4 py-10 bg-primary">
+    <div className="max-w-4xl mx-auto px-4 py-10 ">
       {/* Hero Section */}
       <Hero />
 
