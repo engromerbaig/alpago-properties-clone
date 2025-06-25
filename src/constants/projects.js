a3bde414addb9db9054738154b361e60f6c6d3ca
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-25 20:57:41 +0500

    ADDED

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,32 +1,46 @@
 import placeholder from "@/assets/images/placeholder.webp"
 
+import image1 from "@/assets/images/projects/1.webp";
+import image2 from "@/assets/images/projects/2.webp";
+import image3 from "@/assets/images/projects/3.webp";
+import image4 from "@/assets/images/projects/4.webp";
+import image5 from "@/assets/images/projects/5.webp";
+import image6 from "@/assets/images/projects/6.webp";
+import image7 from "@/assets/images/projects/7.webp";
+
+
 export const PROJECTS_DATA = [
   {
-    name: "Project One",
-    image: placeholder
+    name: "KURAL VISTA",
+    image: image1
   },
   {
-    name: "Project Two", 
-    image: placeholder
+    name: "FRAMED ALLURE", 
+    image: image2
   },
 
    {
-    name: "Project Three", 
-    image: placeholder
+    name: "SERENE VERSANTE", 
+    image: image3
   },
 
    {
-    name: "Project Four", 
-    image: placeholder
+    name: "RIVA DEL LUSSO", 
+    image: image4
   },
 
      {
-    name: "Project Five", 
-    image: placeholder
+    name: "CASA DEL SOLE", 
+    image: image5
   },
 
    {
-    name: "Project Six", 
-    image: placeholder
+    name: "PALM FLOWER", 
+    image: image6
+  },
+
+    {
+    name: "SOLE MARE",
+    image: image7
   }
 ];
