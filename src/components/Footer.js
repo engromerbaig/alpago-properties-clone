1be76b537737faf3f5221b9c92434b146b07574c
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-25 12:19:23 +0500

    added footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 text-center text-sm text-gray-600 py-4 mt-10">
+    <footer className="bg-gray-400 text-center text-sm text-gray-600 py-4 mt-10">
       © {new Date().getFullYear()} JobSite. All rights reserved.
     </footer>
   );
