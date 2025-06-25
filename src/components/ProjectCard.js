4b00320dd0f685296dbda895827090c3286904b3
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-25 20:49:32 +0500

    added scale

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -21,22 +21,23 @@ export default function ProjectCard({ name, image, link = "#", cardHeight='h-[40
             src={image}
             alt={name}
             fill
-            className="object-cover transition-transform duration-500 group-hover:scale-105"
+            className="object-cover transition-transform duration-500 group-hover:scale-115"
             placeholder="blur"
             sizes="(max-width: 768px) 100vw, 50vw"
           />
         </div>
 
-        {/* Icon */}
-        <div className="absolute inset-0 flex items-center justify-center z-10">
-          <div className="bg-[#5B5B5B] rounded-full p-3 opacity-100 transition-opacity duration-300">
-            {isNews ? (
-              <FaPlus className="text-white text-2xl" />
-            ) : (
-              <GoArrowUpRight className="text-white text-2xl" />
-            )}
-          </div>
-        </div>
+        {/* Icon Container Scale */}
+  <div className="absolute inset-0 flex items-center justify-center z-10">
+  <div className="bg-[#5B5B5B] rounded-full p-3 transition-transform duration-500 transform group-hover:scale-120">
+    {isNews ? (
+      <FaPlus className="text-white text-2xl" />
+    ) : (
+      <GoArrowUpRight className="text-white text-2xl" />
+    )}
+  </div>
+</div>
+
 
         {/* Text + Date */}
         <div className="py-4 flex justify-between items-center">
