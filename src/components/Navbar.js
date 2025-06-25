ceb06a91176b243fa072d8eb7abd0af0b6674b5e
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-26 02:15:09 +0500

    added navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,36 @@
+'use client';
+
 import Link from 'next/link';
-import { theme } from '@/theme';
 import Container from './Container';
-
-
+import NAV_LINKS from '@/constants/navlinks';
 
 export default function Navbar() {
   return (
-    <nav className={`absolute  py-6 z-50 shadow-sm w-full`}>
+    <nav className="absolute py-6 z-50 shadow-sm w-full">
       {/* Background layer */}
       <div className="bg-black/30 w-full h-full absolute inset-0 -z-10" />
 
       <Container>
-    <div className="flex justify-between items-center relative z-10">
-        <Link href="/" className="text-xl font-semibold flex items-center">
-          <img src="/logo.png" alt="JobSite Logo" className="h-12 mr-2" />
-        </Link>
-        <div className="space-x-4 uppercase">
-          <Link href="/" className="text-white hover:text-blue-600">Home</Link>
-          <Link href="/about" className="text-white hover:text-blue-600">About</Link>
-                    <Link href="/about" className="text-white hover:text-blue-600">Media</Link>
+        <div className="flex justify-between items-center relative z-10">
+          {/* Logo */}
+          <Link href="/" className="text-xl font-semibold flex items-center">
+            <img src="/logo.png" alt="JobSite Logo" className="h-12 mr-2" />
+          </Link>
 
-          <Link href="/contact" className="text-white hover:text-blue-600">Contact Us</Link>
-                    <Link href="/contact" className="text-white hover:text-blue-600">Expertise</Link>
-
-
+          {/* Navigation Links */}
+          <div className="space-x-4 uppercase">
+            {NAV_LINKS.map((link, idx) => (
+              <Link
+                key={idx}
+                href={link.href}
+                className="text-white hover:text-blue-600"
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-
       </Container>
-
-  
     </nav>
   );
 }
---
+++ src/constants/navlinks.js
@@ -0,0 +1,9 @@
+const NAV_LINKS = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "#" },
+  { name: "Media", href: "#" },
+  { name: "Contact Us", href: "#" },
+  { name: "Expertise", href: "#" },
+];
+
+export default NAV_LINKS;
