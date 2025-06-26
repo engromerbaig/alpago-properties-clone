9a2424429d4f29b697f86400082f1568ff42bbff
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-06-26 13:56:39 +0500

    simplified heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -11,17 +11,9 @@ export default function Heading({
   size = 'text-5xl',
   centered = true,
   fontWeight = 'font-semibold',
-  inActiveHeading = false,
   breakSpan = false,
   className = '',
 }) {
-  // Apply 20% opacity if inActiveHeading is true
-  const getColorWithOpacity = (colorClass) => {
-    if (!inActiveHeading) return colorClass;
-    const colorName = colorClass.replace('text-', '');
-    return `text-${colorName}/20`;
-  };
-
   // Split text into parts around spanText
   const parts = spanText ? text.split(spanText) : [text];
 
@@ -29,7 +21,7 @@ export default function Heading({
   const renderHeadingContent = () => {
     if (!spanText) {
       return (
-        <span className={`${getColorWithOpacity(color)}`}>
+        <span className={color}>
           {text}
         </span>
       );
@@ -38,7 +30,7 @@ export default function Heading({
     return (
       <>
         {parts[0] && (
-          <span className={`${getColorWithOpacity(color)}`}>
+          <span className={color}>
             {parts[0]}
           </span>
         )}
@@ -46,18 +38,18 @@ export default function Heading({
         {breakSpan ? (
           <>
             <br />
-            <span className={`${getColorWithOpacity(spanColor)} ${spanSize} ${spanFontWeight} block`}>
+            <span className={`${spanColor} ${spanSize} ${spanFontWeight} block`}>
               {spanText}
             </span>
           </>
         ) : (
-          <span className={`${getColorWithOpacity(spanColor)} ${spanSize} ${spanFontWeight}`}>
+          <span className={`${spanColor} ${spanSize} ${spanFontWeight}`}>
             {spanText}
           </span>
         )}
         
         {parts[1] && (
-          <span className={`${getColorWithOpacity(color)}`}>
+          <span className={color}>
             {parts[1]}
           </span>
         )}
@@ -89,7 +81,6 @@ Heading.propTypes = {
   size: PropTypes.string,
   centered: PropTypes.bool,
   fontWeight: PropTypes.string,
-  inActiveHeading: PropTypes.bool,
   breakSpan: PropTypes.bool,
   className: PropTypes.string,
 };
